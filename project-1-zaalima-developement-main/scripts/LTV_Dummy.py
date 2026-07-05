d8bad0e55f777191b2577d4b627a8c7c140730fd
Amenda Das <amendadasputhoor@gmail.com>
2026-07-05 16:06:33 +0530

    update

--- project-1-zaalima-developement-main/scripts/LTV_Dummy.py
+++ project-1-zaalima-developement-main/scripts/LTV_Dummy.py
@@ -1,43 +1,25 @@
-import warnings
-
-warnings.filterwarnings("ignore")
-
-
-
-import numpy as np
-
 import pandas as pd
 
-
-
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
-
-from sklearn.preprocessing import OneHotEncoder, RobustScaler
-
-from sklearn.compose import ColumnTransformer
-
-from sklearn.pipeline import Pipeline
-
-from sklearn.impute import SimpleImputer
-
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-
-from sklearn.ensemble import (
-
-    RandomForestRegressor,
-
-    ExtraTreesRegressor,
-
-    GradientBoostingRegressor,
-
-    HistGradientBoostingRegressor
-
+df = pd.read_csv(
+    r"D:\zaalima\Customer_Churn_LTV_Project\WA_Fn-UseC_-Telco-Customer-Churn.csv"
 )
 
-from sklearn.linear_model import Ridge, ElasticNet
+df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
+df.dropna(inplace=True)
 
-from sklearn.inspection import permutation_importance
+PROFIT_MARGIN = 0.30
 
-import joblib
+# Historical revenue
+df["Revenue_LTV"] = df["TotalCharges"]
 
-np.random.seed(42)
+# Estimated profit
+df["Profit_LTV"] = df["Revenue_LTV"] * PROFIT_MARGIN
+
+print(df[["customerID", "tenure", "MonthlyCharges",
+          "TotalCharges", "Revenue_LTV", "Profit_LTV"]])
+
+print("\nSummary")
+print(f"Total Revenue LTV : ${df['Revenue_LTV'].sum():,.2f}")
+print(f"Average Revenue LTV : ${df['Revenue_LTV'].mean():,.2f}")
+print(f"Total Profit LTV : ${df['Profit_LTV'].sum():,.2f}")
+print(f"Average Profit LTV : ${df['Profit_LTV'].mean():,.2f}")
