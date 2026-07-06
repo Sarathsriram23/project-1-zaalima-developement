9480c25657e3c0d0d4001d3ad21b3aa3fef0047e
Amenda Das <amendadasputhoor@gmail.com>
2026-07-07 00:09:26 +0530

    update

--- project-1-zaalima-developement-main/scripts/LTV.py
+++ project-1-zaalima-developement-main/scripts/LTV.py
@@ -1,101 +1,20 @@
-df["ExpectedRevenue"] = (
-    df["MonthlyCharges"] *
-    (df["tenure"] + (1 - df["Churn"]) * 12)
+import pandas as pd
+
+df = pd.read_csv(
+    r"D:\zaalima\Customer_Churn_LTV_Project\WA_Fn-UseC_-Telco-Customer-Churn.csv"
 )
 
-active_df = df[df["Churn"] == 0].copy()
+df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
+df.dropna(subset=["TotalCharges"], inplace=True)
 
-active_df
+# Revenue-based estimated LTV
+df["Estimated_LTV"] = df["MonthlyCharges"] * df["tenure"]
 
-active_df["ExpectedRevenue"] = (
-    active_df["MonthlyCharges"] *
-    (active_df["tenure"] + 12)
-)
+print(df[["customerID",
+          "tenure",
+          "MonthlyCharges",
+          "Estimated_LTV"]])
 
-X = active_df.drop(
-    ["ExpectedRevenue", "Churn"],
-    axis=1
-)
-
-y = active_df["ExpectedRevenue"]
-
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
-)
-
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-from xgboost import XGBRegressor
-
-models = {
-
-    "Linear Regression": LinearRegression(),
-
-    "Random Forest": RandomForestRegressor(
-        n_estimators=200,
-        random_state=42
-    ),
-
-    "XGBoost": XGBRegressor(
-        n_estimators=300,
-        learning_rate=0.05,
-        random_state=42
-    )
-}
-
-pipeline = Pipeline([
-    ("preprocessor", preprocessor),
-    ("regressor", model)
-])
-
-from sklearn.metrics import (
-    mean_absolute_error,
-    mean_squared_error,
-    r2_score
-)
-
-for name, model in models.items():
-
-    pipeline = Pipeline([
-        ("preprocessor", preprocessor),
-        ("regressor", model)
-    ])
-
-    pipeline.fit(X_train, y_train)
-
-    predictions = pipeline.predict(X_test)
-
-    mae = mean_absolute_error(y_test, predictions)
-    rmse = mean_squared_error(
-        y_test,
-        predictions,
-        squared=False
-    )
-    r2 = r2_score(y_test, predictions)
-
-    print(name)
-    print("MAE :", mae)
-    print("RMSE:", rmse)
-    print("R²  :", r2)
-
-    best_rmse = float("inf")
-
-    explainer = shap.LinearExplainer(
-    model,
-    X_test_transformed
-)
-
-active_df["Predicted_LTV"] = best_pipeline.predict(
-    X
-)
-
-active_df["LTV_Segment"] = pd.qcut(
-    active_df["Predicted_LTV"],
-    q=3,
-    labels=["Low", "Medium", "High"]
-)
+print("\n----- Estimated LTV Summary -----")
+print(f"Average Estimated LTV : ${df['Estimated_LTV'].mean():,.2f}")
+print(f"Maximum Estimated LTV : ${df['Estimated_LTV'].max():,.2f}")
